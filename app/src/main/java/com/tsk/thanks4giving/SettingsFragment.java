33af74e518ff6acbddb7ff1c2d08c36280e4bb50
Shlomi <Shlomixg@gmail.com>
2020-10-03 13:19:40 +0300

    fix settings fragment

--- app/src/main/java/com/tsk/thanks4giving/SettingsFragment.java
+++ app/src/main/java/com/tsk/thanks4giving/SettingsFragment.java
@@ -1,13 +1,44 @@
 package com.tsk.thanks4giving;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.preference.PreferenceFragmentCompat;
 
-public class SettingsFragment extends PreferenceFragmentCompat {
+public class SettingsFragment extends PreferenceFragmentCompat implements SharedPreferences.OnSharedPreferenceChangeListener {
 
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         setPreferencesFromResource(R.xml.root_preferences, rootKey);
+    }
+
+    /**
+     * Change preferences selections on live.
+     * This functions uses utils' helper func "findPref" to avoid duplicated code
+     *
+     * @param sharedPrefs The shared preferences of the app
+     * @param key         Key of the changed preference
+     */
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPrefs, String key) {
+        Utils.findPref(sharedPrefs, key);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        getPreferenceManager().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onPause() {
+        getPreferenceManager().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(this);
+        super.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        getPreferenceScreen().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(this);
+        super.onDestroy();
     }
 }
